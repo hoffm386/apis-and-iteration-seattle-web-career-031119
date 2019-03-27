b42e5609d19377965d27434de7e875c829faa367
Erin Hoffman <erinrhof@uw.edu>
2019-03-26 17:32:55 -0700

    Update command_line_interface.rb

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,6 +3,11 @@ def welcome
   puts "Welcome"
 end
 
+def get_movie_from_user
+  puts "please enter a movie name"
+  gets.chomp
+end
+
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
